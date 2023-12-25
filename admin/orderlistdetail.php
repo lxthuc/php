3eab11f3500124a6a12de9e4c8ffc9b99c66e08a
vmhieu1211 <100364773+vmhieu1211@users.noreply.github.com>
2023-12-25 15:28:14 +0700

    Update orderlistdetail.php

--- admin/orderlistdetail.php
+++ admin/orderlistdetail.php
@@ -31,61 +31,61 @@ $order_result = $order->getById($result[0]['orderId']);
 </head>
 
 <body>
-       <div class = "container">
-    <nav>
-        <input type="checkbox" id="check">
-        <label for="check" class="checkbtn">
-            <i class="fas fa-bars"></i>
-        </label>
-        <label class="logo">ADMIN</label>
-        <ul>
-            <li><a href="productlist.php">Quản lý Sản phẩm</a></li>
-            <li><a href="categoriesList.php">Quản lý Danh mục</a></li>
-            <li><a href="orderlist.php" class="active">Quản lý Đơn hàng</a></li>
-        </ul>
-    </nav>
-    <div class="title">
-        <h1>Chi tiết đơn đặt hàng <?= $order_result['id'] ?></h1>
-    </div>
-    <div class="add">
-        <?php
-        if ($result) { ?>
-            <table class="list">
-                <tr>
-                    <th>STT</th>
-                    <th>Tên sản phẩm</th>
-                    <th>Hình ảnh</th>
-                    <th>Đơn giá</th>
-                    <th>Số lượng</th>
-                </tr>
-                <?php $count = 1;
-                foreach ($result as $key => $value) { ?>
-                    <tr>
-                        <td><?= $count++ ?></td>
-                        <td><?= $value['productName'] ?></td>
-                        <td><img class="image-cart" src="uploads/<?= $value['productImage'] ?>" alt=""></td>
-                        <td><?= $value['productPrice'] ?></td>
-                        <td><?= $value['qty'] ?></td>
+    <div class="container">
+        <nav>
+            <a class="logo" href="index.php">
+                <img style="margin-left:50px;width:80px; float:left" e src="img/admin.png"></img>
+            </a>
+            <ul>
+                <li><a href="productlist.php">Quản lý Sản phẩm</a></li>
+                <li><a href="categoriesList.php">Quản lý Danh mục</a></li>
+                <li><a href="orderlist.php" class="active">Quản lý Đơn hàng</a></li>
+                <li><a href="userList.php">Quản lý tài khoản</a></li>
 
+            </ul>
+        </nav>
+        <div class="title">
+            <h1>Chi tiết đơn đặt hàng <?= $order_result['id'] ?></h1>
+        </div>
+        <div class="add">
+            <?php
+            if ($result) { ?>
+                <table class="list">
+                    <tr>
+                        <th>STT</th>
+                        <th>Mã khách hàng</th>
+                        <th>Tên sản phẩm</th>
+                        <th>Hình ảnh</th>
+                        <th>Đơn giá</th>
+                        <th>Số lượng</th>
+                        <th>Địa chỉ</th>
                     </tr>
+                    <?php $count = 1;
+                    foreach ($result as $key => $value) { ?>
+                        <tr>
+                            <td><?= $count++ ?></td>
+                            <td><?= $value['userId']?></td>
+                            <td><?= $value['productName'] ?></td>
+                            <td><img class="image-cart" src="uploads/<?= $value['productImage'] ?>" alt=""></td>
+                            <td><?= $value['productPrice'] ?></td>
+                            <td><?= $value['qty'] ?></td>
+                            <td><?= $value['address']?></td>
+
+                        </tr>
+                    <?php }
+                    ?>
+                </table>
+                <?php
+                if ($order_result['status'] == 'Processing') { ?>
+                    <a href="processed_order.php?orderId=<?= $_GET['orderId'] ?>">Xác nhận</a>
                 <?php }
                 ?>
-            </table>
-            <?php
-            if ($order_result['status'] == 'Processing') { ?>
-                <a href="processed_order.php?orderId=<?= $_GET['orderId'] ?>">Xác nhận</a>
+            <?php } else { ?>
+                <h3>Chưa có đơn hàng nào đang xử lý</h3>
             <?php }
             ?>
-        <?php } else { ?>
-            <h3>Chưa có đơn hàng nào đang xử lý</h3>
-        <?php }
-        ?>
+        </div>
     </div>
-    </div>
-    
-    <footer>
-        <p class="copyright">VMHIEU</p>
-    </footer>
 </body>
 
 </html>

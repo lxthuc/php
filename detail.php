482e7d04080fc3c9c31877f97e832841ded9cd14
lxthuc <thuc2003v4@gmail.com>
2023-12-25 15:49:22 +0700

    11

--- detail.php
+++ detail.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'lib/session.php';
 include 'classes/product.php';
 include_once 'classes/cart.php';
 
@@ -21,19 +22,27 @@ if (!$result) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="https://use.fontawesome.com/2145adbb48.js"></script>
+    <script src="https://kit.fontawesome.com/a42aeb5b72.js" crossorigin="anonymous"></script>
     <title><?= $result['name'] ?></title>
 </head>
 
 <body>
     <nav>
         <a class="logo" href="index.php">
-            <img src="images/logo.png" alt="">
+            <img src="public/images/logo.png" alt="">
         </a>
         <ul>
             <li><a href="index.php">Trang chủ</a></li>
-            <li><a href="productList.php">Sản phẩm</a></li>
-            <li><a href="register.php" id="signup">Đăng ký</a></li>
-            <li><a href="login.php" id="signin">Đăng nhập</a></li>
+            <li><a href="productList.php" class="active">Sản phẩm</a></li>
+            <?php
+            if (isset($_SESSION['user']) && $_SESSION['user']) { ?>
+                <li><a href="logout.php" id="signin">Đăng xuất</a></li>
+            <?php } else { ?>
+                <li><a href="register.php" id="signup">Đăng ký</a></li>
+                <li><a href="login.php" id="signin">Đăng nhập</a></li>
+            <?php } ?>
             <li><a href="order.php" id="order">Đơn hàng</a></li>
             <li>
                 <a href="checkout.php">
